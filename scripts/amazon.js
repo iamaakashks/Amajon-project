32818490bdb71d1eb5e5a45502924b70b4fb0a69
Aakash <aakashkumarsumanaksgi7@gmail.com>
2024-07-01 11:31:23 +0530

    fixed the error occured with the radio

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
+ 
 let productsHTML = "";
 products.forEach(function(products){
     productsHTML += `
@@ -22,7 +24,7 @@ products.forEach(function(products){
           </div>
 
           <div class="product-price">
-            $${(products.priceCents/100).toFixed(2)}
+            $${formatCurrency(products.priceCents)}
           </div>
 
           <div class="product-quantity-container">
